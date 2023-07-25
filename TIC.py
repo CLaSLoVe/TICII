a38965770d600fb7a7ef56170283db627e1d3b1d
CLaSLoVe <jindian_0805@foxmail.com>
2023-07-25 18:38:39 +0800

    why???

--- TIC.py
+++ TIC.py
@@ -51,6 +51,35 @@ class TIC(nn.Module):
         return torch.softmax(lle, dim=-1)
 
 
+class ADMM(torch.optim.Optimizer):
+    def __init__(self, params, rho=1.0, lr=1e-3):
+        defaults = dict(lr=lr)
+        super().__init__(params, defaults)
+        self.rho = rho
+
+        self.z = [torch.zeros_like(p) for p in params]
+        self.u = [torch.zeros_like(p) for p in params]
+
+    def step(self, closure=None):
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        rho = self.rho
+        for i, group in enumerate(self.param_groups):
+            lr = group['lr']
+            params = group['params']
+            for j, p in enumerate(params):
+                z = self.z[j]
+                u = self.u[j]
+                p.data = (z - u) / rho
+                p.data = torch.sign(p.data) * torch.max(torch.abs(p.data) - lr / rho, torch.zeros_like(p.data))
+                z.data = p.data + u
+                u.data = u.data + p.data - z.data
+
+        return loss
+
+
 criterion = nn.CrossEntropyLoss()
 l1_lambda = .01
 
@@ -80,14 +109,15 @@ if __name__ == '__main__':
     num_sensors = 12
     delta_t = 3
     num_clusters = 4
-    num_epoch = 10
+    num_epoch = 1
 
     dataset = XplaneDataset(delta_t)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
     net = TIC(num_sensors, delta_t, num_clusters)
     net = net.to(device)
-    updater = torch.optim.Adam(net.parameters(), lr=0.01)
+    # updater = torch.optim.Adam(net.parameters(), lr=0.01)
+    updater = ADMM(net.parameters(), rho=1.0)
 
     for i in range(num_epoch):
         sum_loss = 0
@@ -100,7 +130,7 @@ if __name__ == '__main__':
             updater.zero_grad()
             l.backward()
             updater.step()
-
+            # break
         for batch in dataloader:
             X, y = batch
             X, y = X.to(device), y.to(device)
@@ -113,11 +143,11 @@ if __name__ == '__main__':
         print('epoch', i+1, ':', (correct/len(dataset.X))*100, '%')
         print('\tloss:', sum_loss/len(dataset.X))
 
-    matrix=-abs(net.mat[0].detach().cpu().numpy())
-    cmap = 'gray'
-    norm = colors.Normalize(vmin=0, vmax=matrix.max())
-    plt.imshow(matrix, cmap=cmap, norm=norm)
-    # 显示颜色条
-    plt.colorbar()
-    # 显示图像
-    plt.show()
+    # matrix=-abs(net.mat[0].detach().cpu().numpy())
+    # cmap = 'gray'
+    # norm = colors.Normalize(vmin=0, vmax=matrix.max())
+    # plt.imshow(matrix, cmap=cmap, norm=norm)
+    # # 显示颜色条
+    # plt.colorbar()
+    # # 显示图像
+    # plt.show()
